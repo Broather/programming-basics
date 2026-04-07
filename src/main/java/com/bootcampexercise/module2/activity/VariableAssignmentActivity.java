abb75c387a50c47a8eb3d50e44654ea8f975a78f
broather <markussgr@gmail.com>
2026-04-07 14:22:39 +0300

    add first activity about variable assignment

---
+++ src/main/java/com/bootcampexercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,13 @@
+package com.bootcampexercise.module2.activity;
+
+public class VariableAssignmentActivity {
+    /**
+     * @author me
+     * @purpose declare, assign and print a variable
+     */
+    static void main() {
+        int a;
+        a = 1;
+        System.out.println(a);
+    }
+}
